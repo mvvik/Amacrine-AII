c6c4562a020c7feb332c1d55918abc8c69e6cecd
Victor V. Matveev <31737482+mvvik@users.noreply.github.com>
2025-06-25 19:30:46 -0400

    Update RUN_THIS_FIRST.m

--- FIGURE_S4_1pool_NonCooperative/RUN_THIS_FIRST.m
+++ FIGURE_S4_1pool_NonCooperative/RUN_THIS_FIRST.m
@@ -2,7 +2,7 @@
 %  Main script to produce all figures for the model in the folder title
 %  Compute Likelihood Profile and plot results
 %
-%  Note: No. of Ca2+ bindings sites is set by nCaSites in ComputeSettings.m 
+%  Note: No. of Ca2+ bindings sites is set by nCaSites in ComputeSetup.m 
 %
 %  Make sure you download the MATLAB Parallel Toolbox
 %  Likelihood profile may take 1-2 hours on a 10-core CPU
