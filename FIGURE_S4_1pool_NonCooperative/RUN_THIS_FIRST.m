9ba63b82d9dad743d4954c76c8eb8835259a03ac
Victor V. Matveev <31737482+mvvik@users.noreply.github.com>
2025-06-25 18:32:46 -0400

    Add files via upload

---
+++ FIGURE_S4_1pool_NonCooperative/RUN_THIS_FIRST.m
@@ -0,0 +1,12 @@
+%
+%  Main script to produce all figures for the model in the folder title
+%  Compute Likelihood Profile and plot results
+%
+%  Note: No. of Ca2+ bindings sites is set by nCaSites in ComputeSettings.m 
+%
+%  Make sure you download the MATLAB Parallel Toolbox
+%  Likelihood profile may take 1-2 hours on a 10-core CPU
+
+Likelihood_Profile;  % Code will automatically load from COMMON folder
+Figure_5B_S4B;       % Code in this folder
+Figure_5C;           % Code will automatically load from COMMON folder
---
+++ FIGURE_S4_1pool_NonCooperative/SetParamBounds.m
@@ -0,0 +1,31 @@
+function Y = SetParamBounds(X, invFlag)
+%
+% "Soft" clipping to upper and lower bounds on parameter values:
+% ------------------------------------------------------------------
+%       X: Parameter array
+%  Rpatch: physical domain size (limiting the 1st two parameters)
+% invFlag: flag for forward vs. backward "soft-clipping" transform
+% ------------------------------------------------------------------
+% 1-pool non-cooperative exocytosis sensor parameters:
+%     1     2       3     4    
+%    R1  SCaKD1  SCakp1 gamma   
+    
+Rpatch   = 0.25;  % Simulation domain radius
+ParamMin = [   0.003,   0.05,  0.001,  0.001];
+ParamMax = [  Rpatch,    100,      1,    100];
+
+Y = abs( X );
+
+SoftMin  = @(x, xMin, g) xMin ./ g(xMin ./ x);
+SoftMax  = @(x, xMax, g) xMax .* g(x ./ xMax);
+
+if invFlag == 0
+    Y = SoftMin(Y, ParamMin, @tanh );
+    Y = SoftMax(Y, ParamMax, @tanh );
+else
+    Y = SoftMax(Y, ParamMax, @atanh );
+    Y = SoftMin(Y, ParamMin, @atanh );
+end
+
+Y           = abs(Y);
+Y(isnan(Y)) = ParamMin(isnan(Y));
