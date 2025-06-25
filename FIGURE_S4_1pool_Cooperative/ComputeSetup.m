415e6c78894e78ca028e90502b10eb7970639d39
Victor V. Matveev <31737482+mvvik@users.noreply.github.com>
2025-06-25 18:34:01 -0400

    Add files via upload

---
+++ FIGURE_S4_1pool_Cooperative/ComputeSetup.m
@@ -0,0 +1,58 @@
+%
+% Global definitions: exocytosis model type, etc.
+% -------------------------------------------------
+%  1-pool cooperative exocytosis sensor parameters:
+%     1     2       3     4    5 
+%    R1  SCaKD1  SCakp1 coop gamma
+
+addpath('../COMMON/');
+CommonParameters;     % Get common simulation parameters
+ComputeCalciumPDE;
+
+nPars      = 5;       % Number of exocytosis model parameters
+nCaSites   = 5;       % Number of Ca binding sites in exocytosis sensor (2..5)
+DataPrefix = ['Coop_1pool_CaSites', num2str(nCaSites), '_']; 
+
+% --- Generate exocytosis data curve that the model will attempt fitting:
+
+DT                = logspace(log10(0.5), log10(totalTime), nPoints); 
+[DataIn, ErrorIn] = GenerateExocytosisData( DT, 0);
+
+% --- Read computed [Ca2+] vs distance and time, for each buffering condition:
+
+[  ~,    CaGrid1, tArray1] = ReadDataVsTime(fileEGTA2mM );
+[  ~,    CaGrid2, tArray2] = ReadDataVsTime(fileEGTA10mM);
+[rArray, CaGrid3, tArray3] = ReadDataVsTime(fileBAPTA1mM);
+
+% --- Generate fprintf format string for parameter output:
+
+fStr = '';
+for ind = 1 : nPars
+   fStr = [fStr, '%g '];
+end
+
+% --- Select the model integrator based on the number of sensor's Ca2+ binding sites:
+
+GetModel = str2func( strcat('@GetModelOutput',         num2str(nCaSites)) );
+GetEquil = str2func( strcat('@(X) getEquilibriumCoop', num2str(nCaSites), '(X(2), X(4))') );
+CostFunc = @(X) CostFunction(GetModel, GetEquil, X, tArray1, tArray2, tArray3, CaGrid1, CaGrid2, CaGrid3, DataIn, ErrorIn, rArray, DT);
+
+% --- Parameter windows to scan:
+
+switch nCaSites
+    case 2
+        Xmin = [ 0.035  0.5    0.4   0.003   1];
+        Xmax = [ 0.050    4   0.99   0.50   10];
+    case 3
+        Xmin = [ 0.025   1     0.4   0.01   1];
+        Xmax = [ 0.040   7     0.99   0.3  10];
+    case 4
+        Xmin = [ 0.020   3     0.4   0.02   1];
+        Xmax = [ 0.030  11     0.99  0.50  15];
+    case 5
+        Xmin = [ 0.015   4     0.4   0.10   1];
+        Xmax = [ 0.026  14     0.99  0.55  30];
+end  
+
+maxCost = 180;
+
---
+++ FIGURE_S4_1pool_Cooperative/Figure_5B_S4B.m
@@ -0,0 +1,132 @@
+function Figure_5B_S4B()
+
+% -------------------------------------------------------------------------
+%   Figure 5B/S4B: Parameter profile plots (max likelihood profile) 
+% -------------------------------------------------------------------------
+
+ComputeSetup;
+ResultsOut = CollectParForData(nPars);
+BestError  = 48.3216;  % Smallest error: 2-pool cooperative model with 5 bindings sites
+
+% Parameter labels and units
+labels = {'R_X', 'K_D', 'k_{on}', '\beta', '\gamma'};
+Units  = {'nm', '\muM', '(\muMms)^{-1}', '', 'ms^{-1}'};
+
+Order    = [1 2 3 4 5];   % Order of parameters for display
+nRows    = 2;
+nCols    = 3;
+nBins    = 30;
+clr      = [0.6, 0.0, 0.6];  % Fill color
+tfs      = 12;               % Title font size
+indGamma = 5;                % Use log scale for parameter #5 = gamma
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Preprocess and filter results
+
+% Rescale first parameter to nm, apply parameter bounds
+ResultsOut(:, 1:nPars) = SetParamBounds(ResultsOut(:, 1:nPars), 0);
+ResultsOut(:, 1)       = ResultsOut(:, 1) * 1000;
+ResultsOut(:, end)     = ResultsOut(:, end) - BestError; 
+
+% Find minimum cost (LLR) and corresponding parameters
+[Vmin, Imin] = min(ResultsOut(:, end));
+ParMin       = ResultsOut(Imin, 1:nPars);
+
+fprintf(['Minimal error = %g \nRelative to best = %g \n Pars = %g ', fStr, '\n'], Vmin + BestError, Vmin, ParMin);
+
+% Apply cutoff for accepted solutions
+MaxError           = Vmin + LLR_CutOff;
+validIdx           = ResultsOut(:, end) <= MaxError;
+ResultsOut         = ResultsOut(validIdx, :);
+
+fprintf('Filtered set: N = %d\n', size(ResultsOut, 1));
+
+% Reorder parameters for plotting convenience
+ResultsOut(:, 1:nPars) = ResultsOut(:, Order);
+ParMin                 = ParMin(Order);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Profile likelihood plots for each parameter
+
+figure;
+
+for indPar = 1:nPars
+    % Bin parameter values
+    Xmin = min(ResultsOut(:, indPar));
+    Xmax = max(ResultsOut(:, indPar));
+    valPars = zeros(1, nBins);
+    valErrs = zeros(1, nBins);
+
+    % Optional log-scaling for parameter 8 (can be skipped here)
+    useLog = (indPar == indGamma);
+    if useLog
+        Xmin = log(Xmin);
+        Xmax = log(Xmax);
+    end
+
+    for bin = 1:nBins
+        % Define bin range
+        val0 = Xmin + (bin - 1) / nBins * (Xmax - Xmin);
+        val1 = Xmin +  bin      / nBins * (Xmax - Xmin);
+        valC = 0.5 * (val0 + val1);
+
+        if useLog
+            val0 = exp(val0); val1 = exp(val1); valC = exp(valC);
+        end
+
+        valPars(bin) = valC;
+
+        % Extract results within this bin
+        inBin = ResultsOut(:, indPar) >= val0 & ResultsOut(:, indPar) < val1;
+        if any(inBin)
+            valErrs(bin) = min(ResultsOut(inBin, end));
+        else
+            % Nearest fallback
+            [~, closest] = min((ResultsOut(:, indPar) - valC).^2);
+            valErrs(bin) = ResultsOut(closest, end);
+        end
+    end
+
+    % Restore linear range if log-scaling was used
+    if useLog
+        Xmin = exp(Xmin);
+        Xmax = exp(Xmax);
+    end
+
+    % Pad ends to close polygon
+    valPars = [valPars(1), valPars, valPars(end)];
+    valErrs = [Vmin+LLR_CutOff, valErrs, Vmin+LLR_CutOff];
+
+    subplot(nRows, nCols, indPar); hold on;
+    fill(valPars, valErrs, clr, 'EdgeColor', clr, 'LineWidth', 1);
+    axis([Xmin Xmax Vmin Vmin+LLR_CutOff]);
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Add vertical lines and axis formatting
+
+for nn = 1:nPars
+    subplot(nRows, nCols, nn);
+    parVal = ParMin(nn);
+
+    % Vertical line at optimal parameter
+    plot(parVal * [1 1], [Vmin Vmin+LLR_CutOff], 'w:', 'LineWidth', 2);
+
+    % Annotated title
+    if parVal < 10
+        strTitle = sprintf('%s = %.2g %s', labels{nn}, parVal, Units{nn});
+    else
+        strTitle = sprintf('%s = %.0f %s', labels{nn}, parVal, Units{nn});
+    end
+    title(strTitle, 'FontSize', tfs, 'Color', 'k');
+
+    % Reverse Y-axis to match standard profile-likelihood plot style
+    set(gca, 'YDir', 'reverse');
+
+    % Log x-axis scaling for last parameter (assumed gamma)
+    if nn == indGamma
+        set(gca, 'XScale', 'log');
+        set(gca, 'XTick', [3 5 10 20 50]);
+    end
+    %axis tight;
+end
