d35e54d757d415f97cb11efda4f45e627c3773d2
Victor V. Matveev <31737482+mvvik@users.noreply.github.com>
2025-06-26 10:24:54 -0400

    Update ComputeSetup.m

--- FIGURE_S4_1pool_NonCooperative/ComputeSetup.m
+++ FIGURE_S4_1pool_NonCooperative/ComputeSetup.m
@@ -6,7 +6,6 @@
 %    R1  SCaKD1  SCakp1 gamma   
 
 addpath('../COMMON/');
-CommonParameters;     % Get common simulation parameters
 ComputeCalciumPDE;
 
 nPars      = 4;       % Number of exocytosis model parameters
