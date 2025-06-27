d0154406b793d95e8eb48d6b7b9126549d139b49
Victor V. Matveev <31737482+mvvik@users.noreply.github.com>
2025-06-26 20:52:11 -0400

    Update RUN_THIS_FIRST.m

--- FIGURE_5_2pool_Cooperative/RUN_THIS_FIRST.m
+++ FIGURE_5_2pool_Cooperative/RUN_THIS_FIRST.m
@@ -7,6 +7,8 @@
 %  Make sure you download the MATLAB Parallel Toolbox
 %  Likelihood profile may take 1-2 hours on a 10-core CPU
 
+clear;
+ComputeSetup;        % Compute [Ca2+] and all other settings
 Likelihood_Profile;  % Generate parameter profile (code in COMMON folder): takes some time!
 Figure_5B_S4B;       % Plot the final parameter profile
 Figure_5C;           % Plot model Cm vs pulse duration (code in COMMON folder)
