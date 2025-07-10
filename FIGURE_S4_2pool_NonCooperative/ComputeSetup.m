1d9c9a31472a0de6b86b93cf606041efe67488d6
Victor V. Matveev <31737482+mvvik@users.noreply.github.com>
2025-07-10 10:20:44 -0400

    Add files via upload

--- FIGURE_S4_2pool_NonCooperative/ComputeSetup.m
+++ FIGURE_S4_2pool_NonCooperative/ComputeSetup.m
@@ -8,8 +8,8 @@
 addpath('../COMMON/');
 ComputeCalciumPDE;
 
-nPars      = 6;       % Number of exocytosis model parameters
 nCaSites   = 5;       % Number of Ca binding sites in exocytosis sensor (2..5)
+nPars      = 6;       % Number of exocytosis model parameters
 DataPrefix = ['Noncoop_2pool_CaSites', num2str(nCaSites), '_']; 
 
 % --- Generate fprintf format string for parameter output:
@@ -25,27 +25,27 @@ GetModel = str2func( strcat('@GetModelOutput',         num2str(nCaSites)) );
 GetEquil = str2func( strcat('@(X) getEquilibriumCoop', num2str(nCaSites), '(X(3), 1)') );
 CostFunc = @(X) CostFunction(GetModel, GetEquil, X, tArray1, tArray2, tArray3, CaGrid1, CaGrid2, CaGrid3, DataIn, ErrorIn, rArray, DT);
 
-% --- Parameter windows to scan:
+% -- Parameter windows to scan and initial error target before optimization:
 
 switch nCaSites
         case 5
             Xmin = [ 0.026  0.046  0.5   0.5    1   0.54];
             Xmax = [ 0.037  0.066  2.0  0.99   30   0.65];
-            maxCost = 110;
+            maxCost = 70;
         case 4
             Xmin = [ 0.031  0.050  0.6   0.4    1   0.48];
             Xmax = [ 0.040  0.070  1.4  0.99   20   0.70];
-            maxCost = 130;
+            maxCost = 75;
         case 3
             Xmin = [ 0.035  0.060  0.6  0.50   1  0.48];
             Xmax = [ 0.045  0.082  1.5  0.99  20  0.70];
-            maxCost = 150;
+            maxCost = 85;
         case 2
             Xmin = [ 0.028  0.045  0.55  0.55    1   0.48];
             Xmax = [ 0.040  0.065  1.60  0.99   20   0.70];
-            maxCost = 170;
+            maxCost = 110;
 end
 
-Xmin = real( SetParamBounds(Xmin, 1) );  % Invert bound-clipping to ensure entire range is covered
-Xmax = real( SetParamBounds(Xmax, 1) );  % Invert bound-clipping to ensure entire range is covered
+Xmin = real( SetParamBounds(Xmin, 1) );  % Invert bound-clipping to ensure the whole range is covered
+Xmax = real( SetParamBounds(Xmax, 1) );  % Invert bound-clipping to ensure the whole range is covered
 
